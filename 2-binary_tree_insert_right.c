0cdb91d75406645470d33263a4f7ccee6908d447
Gerdia0117 <Gerald.froz@gmail.com>
2025-04-12 15:36:06 -0400

    sending my work

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,29 +2,28 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - Inserts node as right-child of another node
- * @parent: Pointer to the node to insert right-child in
- * @value: Value to store in new node
- *
- * Return: Pointer to created node, or NULL on failure
+ * binary_tree_insert_right - Inserts node as right-child
+ * @parent: Pointer to parent node
+ * @value: Value for new node
+ * Return: New node pointer, NULL on failure
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+    binary_tree_t *new_node;
 
-	if (parent == NULL)
-		return (NULL);
+    if (!parent)
+        return (NULL);
 
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
-		return (NULL);
+    new_node = binary_tree_node(parent, value);
+    if (!new_node)
+        return (NULL);
 
-	if (parent->right != NULL)
-	{
-		new_node->right = parent->right;
-		parent->right->parent = new_node;
-	}
-	parent->right = new_node;
+    if (parent->right)
+    {
+        new_node->right = parent->right;
+        parent->right->parent = new_node;
+    }
+    parent->right = new_node;
 
-	return (new_node);
+    return (new_node);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -1,17 +1,7 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-#include <stddef.h>
-
-/**
- * struct binary_tree_s - Binary tree node
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-typedef struct binary_tree_s
-{
+typedef struct binary_tree_s {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
@@ -20,8 +10,8 @@ typedef struct binary_tree_s
 
 /* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 void _binary_tree_delete(binary_tree_t *tree);
 
-#endif /* BINARY_TREES_H */
+#endif
