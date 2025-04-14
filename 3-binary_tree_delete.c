bb5a0b6a0977cac82b66fec58b9c703b9e2f44ed
Gerdia0117 <Gerald.froz@gmail.com>
2025-04-13 21:54:20 -0400

    pushing work

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,15 +2,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - Deletes an entire binary tree using post-order traversal
- * @tree: Pointer to the root node of the tree to delete
- *
- * Description: Recursively frees memory for all nodes in the tree.
- *              If tree is NULL, does nothing.
+ * binary_tree_delete - Deletes entire binary tree
+ * @tree: Pointer to root node
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return;
 
 	binary_tree_delete(tree->left);
